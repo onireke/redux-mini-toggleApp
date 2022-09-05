e566ed810867482dd09d742368705a2c87b72095
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-09-05 11:58:18 +0100

    feat: update

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
 const Products = (props) => {
+  console.log(props);
+  const DUMMY_PRODUCT = [
+    {
+      id: "p1",
+      price: 6,
+      title: "my first book",
+      description: "the first cart project code i have ever written",
+    },
+    {
+      id: "p2",
+      price: 6,
+      title: "my second book",
+      description: "the second cart project code i have ever written",
+    },
+  ];
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCT.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
